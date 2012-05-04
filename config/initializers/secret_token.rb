5711628adedabffc0840fac863ac9efeec6d8218
jevgeniy <jevgeniy.mogylskiy@perfectial.com>
2012-05-04 16:01:02 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WorkFu::Application.config.secret_token = '0a3f004a5d277337c91192d89ebd7a62cd04f443abf2034852068ef9c61cba2efedfcf1927dd31d30ee575fae489c602d860860380509d4458885f036d00dbd9'
